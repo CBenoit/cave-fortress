01fc1c508b968ead85d40b94733c080bf30f2cb0
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-13 18:34:03 +0200

    Add material resistence system.

--- src/actors/bullets/basic_bullet.gd
+++ src/actors/bullets/basic_bullet.gd
@@ -7,4 +7,4 @@ func _on_colliding():
 		var collision_pos = get_pos()
 		if collider.get_cellv(collider.world_to_map(collision_pos)) == SolidTiles.TILE_EMPTY:
 			collision_pos += Vector2(cos(orientation), -sin(orientation)) * TilemapsConstants.TILE_SIZE / 4
-		collider.damage_tile(collider.world_to_map(collision_pos))
+		collider.damage_tile(collider.world_to_map(collision_pos), power)
--- src/tilesets/solids_constants.gd
+++ src/tilesets/solids_constants.gd
@@ -4,3 +4,11 @@ const TILE_EMPTY = -1
 const TILE_STONE = 0
 const TILE_DIRT = 1
 const TILE_STEEL = 2
+
+const TILE_HEALTH = {
+	TILE_EMPTY: 0,
+	TILE_STONE: 25,
+	TILE_DIRT: 8,
+	TILE_STEEL: 40
+}
+
