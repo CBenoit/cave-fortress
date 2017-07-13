798004e5f801734ba2667d85fd67314cffa00a9c
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-13 11:41:12 +0200

    Update bullet and tiles (preparing destructibles tiles)

--- src/actors/bullets/basic_bullet.gd
+++ src/actors/bullets/basic_bullet.gd
@@ -2,13 +2,21 @@ extends KinematicBody2D
 
 export var orientation = 0.0
 export var speed = 1000
+export var power = 1
 
 func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	# move the bullet
 	var direction = Vector2(cos(orientation), -sin(orientation))
 	move(direction * speed * delta)
 
 	if is_colliding():
-		queue_free()
+		# apply damages to collider if applicable
+		var collider = get_collider()
+		if "solid" in collider.get_groups():
+			collider.damage(power)
+
+		queue_free() # this free the bullet safely
+
--- src/tilesets/solids.gd
+++ src/tilesets/solids.gd
@@ -1,3 +1,15 @@
 extends StaticBody2D
 
-export var health = 10
+export var max_health = 10
+
+var health = max_health
+
+func damage(value):
+	health -= value
+	if health <= 0:
+		queue_free()
+
+func repair(value):
+	health += value
+	if health > max_health:
+		health = max_health
