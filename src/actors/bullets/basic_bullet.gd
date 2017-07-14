a5960427f5811075ef444d57fdeb82a646635916
bloodre <mathieu87100@gmail.com>
2017-07-14 21:34:40 +0200

    shotgun bullets+weapon

--- src/actors/bullets/basic_bullet.gd
+++ src/actors/bullets/basic_bullet.gd
@@ -6,5 +6,5 @@ func _on_colliding():
 	if "solids" in collider.get_groups():
 		var collision_pos = get_pos()
 		if collider.get_cellv(collider.world_to_map(collision_pos)) == SolidTiles.TILE_EMPTY:
-			collision_pos += Vector2(cos(orientation), -sin(orientation)) * TilemapsConstants.TILE_SIZE / 4
+			collision_pos += Vector2(cos(orientation), -sin(orientation)) * TilemapsConstants.TILE_SIZE / 8
 		collider.damage_tile(collider.world_to_map(collision_pos), power)
--- src/actors/hero/weapons/abstract_weapon.gd
+++ src/actors/hero/weapons/abstract_weapon.gd
@@ -4,7 +4,7 @@ const AUTOMATIC_MODE = 0
 const SEMI_AUTO_MODE = 1
 
 export(float, 0.01, 15, 0.01) var fire_interval_sec = 0.1
-export(float, 0, 2, 0.01) var fire_angle = 0
+export(float, 0, 2, 0.02) var fire_angle = 0
 export(int,1,5,1) var bullet_num = 1
 export(PackedScene) var bullet_scn
 export(int, "Automatic", "Semi-auto") var mode
@@ -25,14 +25,13 @@ func fire():
 		_create_bullets()
 
 func _create_bullets():
-	# default behaviour: one bullet taking into account the given fire angle
-	var bullet = bullet_scn.instance()
+	# default behaviour: bullets taking into account the given fire angle
 	var bullets = []
 	for i in range(bullet_num):
 		bullets.append(bullet_scn.instance())
 		bullets[i].orientation = get_rot() + rand_range(-fire_angle / 2, fire_angle / 2)
 		bullets[i].set_pos(fire_position.get_global_pos())
 		bullets[i].add_collision_exception_with(get_node("..")) # avoid collision with the shooter...
-		for j in range(i):
+		for j in range(i): # removing collisions with previously created bullets
 			bullets[i].add_collision_exception_with(bullets[j])
 		get_node("../..").add_child(bullets[i]) # add the bullets to the node "entities" in abstract_level
