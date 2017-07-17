475e713ea085fefb6f9fc87559cdc0218731a95e
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-17 18:28:10 +0200

    Improve HUD. Debug is now toggleable with F1. Displays FPS too.

--- src/levels/HUD.gd
+++ src/levels/HUD.gd
@@ -1,9 +1,24 @@
 extends CanvasLayer
 
+var debug_node
 var current_weapon_label
+var fps_label
 
 func _ready():
-	current_weapon_label = get_node("current_weapon")
+	debug_node = get_node("debug")
+	current_weapon_label = get_node("debug/vbox/current_weapon")
+	fps_label = get_node("debug/vbox/fps")
+
+	set_process(!debug_node.is_hidden())
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action("display_debug") && event.is_pressed():
+		debug_node.set_hidden(!debug_node.is_hidden())
+		set_process(!debug_node.is_hidden())
+
+func _process(delta):
+	fps_label.set_text("FPS: %d" % Performance.get_monitor(Performance.TIME_FPS))
 
 func current_weapon_changed(new_weapon):
-	current_weapon_label.set_text("Weapon: " + str(new_weapon.name))
+	current_weapon_label.set_text("Weapon name: %s" % new_weapon.name)
