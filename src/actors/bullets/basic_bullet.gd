86c00db0f47b3c8c114b400280aa31972c403efe
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-17 10:07:07 +0200

    Bullets now apply impulses to grenades on collision.

--- src/actors/bullets/basic_bullet.gd
+++ src/actors/bullets/basic_bullet.gd
@@ -3,8 +3,11 @@ extends "./abstract_bullet.gd"
 func _on_colliding():
 	# apply damages to collider if applicable
 	var collider = get_collider()
-	if "solids" in collider.get_groups():
-		var collision_pos = get_pos()
-		if collider.get_cellv(collider.world_to_map(collision_pos)) == SolidTiles.TILE_EMPTY:
-			collision_pos += Vector2(cos(orientation), -sin(orientation)) * TilemapsConstants.TILE_SIZE / 4
-		collider.damage_tile(collider.world_to_map(collision_pos), power)
+	for grp in collider.get_groups():
+		if grp == "solids":
+			var collision_pos = get_pos()
+			if collider.get_cellv(collider.world_to_map(collision_pos)) == SolidTiles.TILE_EMPTY:
+				collision_pos += Vector2(cos(orientation), -sin(orientation)) * TilemapsConstants.TILE_SIZE / 4
+			collider.damage_tile(collider.world_to_map(collision_pos), power)
+		elif grp == "grenade":
+			collider.apply_impulse(Vector2(0, 0), Vector2(cos(orientation) * speed * power, -sin(orientation) * speed * power))
