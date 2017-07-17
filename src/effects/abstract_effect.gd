74ce953039ddbfcdcff32f80dbfc60b01c166d4b
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-17 17:18:47 +0200

    Fix explosion sound not being played.

--- src/effects/abstract_effect.gd
+++ src/effects/abstract_effect.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+# activate should be called once the effect has been added to the graph
+# otherwise, curious problems may occur (such as sounds not being played
+# as the the sound node is not yet added to the graph too).
 func activate():
 	_on_activate()
