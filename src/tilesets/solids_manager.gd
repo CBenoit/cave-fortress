6d06c67ac40a878ca6e39e722afc844380fdd959
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-17 18:50:02 +0200

    Fix fissures not being removed when one shooting tiles.

--- src/tilesets/solids_manager.gd
+++ src/tilesets/solids_manager.gd
@@ -10,15 +10,18 @@ func damage_tile(tile_pos, damages):
 
 	if tiles_damages.has(tile_pos):
 		tiles_damages[tile_pos].decrease_tile_health(damages)
+		if tiles_damages[tile_pos].get_tile_health() <= 0:
+			set_cellv(tile_pos, SolidTiles.TILE_EMPTY)
+			tiles_damages.erase(tile_pos) # free the memory
 	else:
-		# create fissures
+		var this_tile_max_health = SolidTiles.TILE_HEALTH[get_cellv(tile_pos)]
+		if this_tile_max_health - damages <= 0:
+			set_cellv(tile_pos, SolidTiles.TILE_EMPTY)
+			return
+
+		# tile is not yet totally destroyed, create fissures
 		var fissures = fissures_scn.instance()
 		fissures.set_pos(map_to_world(tile_pos))
-		var this_tile_max_health = SolidTiles.TILE_HEALTH[get_cellv(tile_pos)]
 		fissures.init(this_tile_max_health, this_tile_max_health - damages)
 		tiles_damages[tile_pos] = fissures
 		add_child(fissures)
-
-	if tiles_damages[tile_pos].get_tile_health() <= 0:
-		set_cellv(tile_pos, SolidTiles.TILE_EMPTY)
-		tiles_damages.erase(tile_pos) # free the memory
