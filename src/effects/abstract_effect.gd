4194ee2badbfe56f522e8fa185c5f3e142822c5b
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-07-18 21:06:31 +0200

    Add expiration timeout to effects.

--- src/effects/abstract_effect.gd
+++ src/effects/abstract_effect.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	get_node("expire_timeout").connect("timeout", self, "queue_free")
+
 # activate should be called once the effect has been added to the graph
 # otherwise, curious problems may occur (such as sounds not being played
 # as the the sound node is not yet added to the graph too).
